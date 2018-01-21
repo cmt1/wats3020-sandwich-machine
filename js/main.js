235715ed60764e7afa83a08621947211c588a4ca
cmt1 <carlostorres@CARLOSs-MacBook-Air.local>
2018-01-20 20:05:09 -0800

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 /* JavaScript for WATS 3020 Sandwich Machine Assignment */
 
-// Step One ///////////////////////////////////////////////////////////
-//
-// Gather data from the user using the prompt() command.
-// Users will fill in each prompt with text.
-// Provide examples where it may help (e.g. show a comma-separated list of// toppings to indicate how to specify toppings, etc.)
+
 
 
 let breadOrder = prompt('What kind of bread would you like? (white,wheat, or flat)', 'wheat');
@@ -29,38 +25,30 @@ let prices = {
     condiment: 0.25 // Each condiment costs $0.25
 };
 
-// TODO: Convert order information from Strings to Arrays.
+
 
 let meatArray = meatOrder.split(',');
 let toppingArray = toppingOrder.split(',');
 let condimentArray = condimentOrder.split(',');
 
-// TODO: Calculate cost for meat, toppings, and condiments.
-// This requires you to determine the length of each Array you just made
-// and multiply out the costs. You will need to refer to the attributes of the
-// `prices` object in order to calculate these costs.
+
 
 let meatCost = meatArray.length * prices.meat;
 let toppingCost = toppingArray.length * prices.topping;
 let condimentCost = condimentArray.length * prices.condiment;
 
-// TODO: Combine the costs of each part of the sandwich to get the subtotal.
+
 let subtotal = prices.sandwich + meatCost + toppingCost + condimentCost;
 
-// TODO: Calculate the tax owed using the waStateTaxRate.
+
 let waStateTaxRate = 0.065;
 let orderTax = subtotal * waStateTaxRate;
 
-// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
+
 let totalPrice = subtotal + orderTax;
 
 
-// Step Three //////////////////////////////////////////////////////////
-//
-// TODO: Now that we've calculated all the values, insert them into this
-// template literal using proper expression tags. Note that we must provide
-// all of the info the user gave us to confirm the order, and then we must also
-// provide the cost information so the user understands their bill.
+
 
 let receiptTemplate = `
     <p>SANDWICH ORDER</p>
